26cbb5aab5ac6eb655e224a4e44a129fd8e325e0
mmongee <115122095+Villarley@users.noreply.github.com>
2025-10-11 23:54:10 +0200

    allegro instalado

--- proyecto_allegro_1/proyecto_allegro_1.cpp
+++ proyecto_allegro_1/proyecto_allegro_1.cpp
@@ -1,19 +1,10 @@
-// proyecto_allegro_1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <allegro5/allegro.h>
+int main() {
+    if (!al_init()) return -1;
+    ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_flip_display();
+    al_rest(2.0);
+    al_destroy_display(display);
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
